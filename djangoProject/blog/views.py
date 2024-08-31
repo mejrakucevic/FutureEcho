4790ff9b31abdf583a6869f3576e06eb88d3df40
mejrakucevic <mejrakucevic420@gmail.com>
2024-08-31 17:48:32 +0200

    .

--- djangoProject/blog/views.py
+++ djangoProject/blog/views.py
@@ -1,59 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-posts = [
-    {
-        'author': 'Mejra K',
-        'title': 'Letter 1',
-        'content': 'In this Python Django Tutorial, we will be learning how to use templates to return more complex HTML to the browser. Well also see how we can pass variables to our templates as context. Lets get started..In this Python Django Tutorial, we will be learning how .',
-        'datePosted': '28.4.2024'
-    },
-    
-        {
-        'author': 'Lottie M',
-        'title': 'Letter 1',
-        'content': 'In this Python Django Tutorial, we will be learning how to use templates to return more complex HTML to the browser. Well also see how we can pass variables to our templates as context. Lets get started...In this Python Django Tutorial, we will be learning how ',
-        'datePosted': '28.4.2024'
-    },
-        
-    {
-        'author': 'Natalie S',
-        'title': 'Letter 1',
-        'content': 'In this Python Django Tutorial, we will be learning how to use templates to return more complex HTML to the browser. Well also see how we can pass variables to our templates as context. Lets get started...In this Python Django Tutorial, we will be learning how ',
-        'datePosted': '28.4.2024'
-    },
-    
-     {
-        'author': 'Mejra',
-        'title': 'Letter 1',
-        'content': 'In this Python Django Tutorial, we will be learning how to use templates to return more complex HTML to the browser. Well also see how we can pass variables to our templates as context. Lets get started...In this Python Django Tutorial, we will be learning how ',
-        'datePosted': '28.4.2024'
-    },
-     
-    {
-        'author': 'Mejra',
-        'title': 'Letter 1',
-        'content': 'In this Python Django Tutorial, we will be learning how to use templates to return more complex HTML to the browser. Well also see how we can pass variables to our templates as context. Lets get started...In this Python Django Tutorial, we will be learning how ',
-        'datePosted': '28.4.2024'
-    },
-    
-     {
-        'author': 'Mejra',
-        'title': 'Letter 1',
-        'content': 'In this Python Django Tutorial, we will be learning how to use templates to return more complex HTML to the browser. Well also see how we can pass variables to our templates as context. Lets get started...In this Python Django Tutorial, we will be learning how ',
-        'datePosted': '28.4.2024'
-    },
-     
-      {
-        'author': 'Mejra',
-        'title': 'Letter 1',
-        'content': 'In this Python Django Tutorial, we will be learning how to use templates to return more complex HTML to the browser. Well also see how we can pass variables to our templates as context. Lets get started...In this Python Django Tutorial, we will be learning how ',
-        'datePosted': '28.4.2024'
-    },
-    
-]
-
+from .models import Post
 
 def home(request):
     return render(request, 'blog/home.html')
@@ -63,7 +10,7 @@ def contact(request):
 
 def letters(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/letters.html', context)
 
